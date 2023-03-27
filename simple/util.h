d7ff9b38f68e0bf7cd7ebf68f4f40667d978177f
Wasin Thonkaew <haxpor@gmail.com>
2023-03-27 15:11:39 +0700

    add function comment included with arguments information for util.h in simple/

--- simple/util.h
+++ simple/util.h
@@ -5,11 +5,22 @@
 namespace util {
 	/**
 	 * Convert via copying from std::string to C-string.
+	 *
+	 * # Arguments
+	 * - str - input string
+	 * - out - destination c-string pointer to copy the content of string to
+	 * - len - length of string to copy from
 	 */
 	void str_to_cstr(const std::string& str, char* out, unsigned len);
 
 	/**
 	 * Convert via copying from std::string to UTF-16 string.
+	 *
+	 * # Arguments
+	 * - str - input string
+	 * - out - destination c-string pointer to copy the content of converted string
+	 *		   of UTF-16 to
+	 * - len - length of string to copy from
 	 */
 	void str_to_cstr_u16(const std::string& str, char* out, unsigned len);
 };
